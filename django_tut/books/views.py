4c952e6f5720827715f2fb49afb3e52867a80c3a
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2023-07-18 01:27:36 +0600

    ghum dhorse

--- django_tut/books/views.py
+++ django_tut/books/views.py
@@ -16,7 +16,7 @@ def index(request):
 
     books_data = []
 
-    for index in books.index:
+    for index in books.head(50).index:
         books_data.append({
             'id': index,
             'title': books['Book-Title'][index],
@@ -25,6 +25,8 @@ def index(request):
             'quantity': 15,
             'price': 15.99
         })
+
+    random.shuffle(books_data)
 
     return render(request, 'books/index.html', context={
         'best_sellers': books_data,
@@ -62,6 +64,8 @@ def book_detail(request, id: int):
         'price': 15.99
     } for index in similar_books.index]
 
+    random.shuffle(recommendation)
+
     return render(request, 'books/book_detail.html', context={
         'book': book,
         'user': {
@@ -76,27 +80,28 @@ def book_suprise(request):
 
     if book_title:
         index = np.where(pt.index == book_title)[0][0]
+        print(index)
         similar_items = sorted(
             list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:18]
 
         similar_book_ids = [item[0] for item in similar_items]
         # print(similar_book_ids)
 
-        similar_books = books.iloc[similar_book_ids]
+        surprise_books = books.iloc[similar_book_ids]
 
-        similar_books = [{
+        surprise_books = [{
             'id': index,
             'title': books['Book-Title'][index],
             'thumbnailurl': books['Image-URL-M'][index],
             'quantity': 15,
             'price': 15.99
-        } for index in similar_books.index]
+        } for index in surprise_books.index]
 
-        print(similar_books)
+        random.shuffle(surprise_books)
 
         return render(request, 'books/surprise.html', {
             'book_title': book_title,
-            'similar_books': similar_books
+            'similar_books': surprise_books
         })
 
     return render(request, 'books/surprise.html')
