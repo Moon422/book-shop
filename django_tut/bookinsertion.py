e876d4e8335fece3e9db5031d49fc899e4093fc2
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2023-07-17 19:25:51 +0600

    product recommendation

--- django_tut/bookinsertion.py
+++ django_tut/bookinsertion.py
@@ -1,1 +1,19 @@
 from books import models
+
+with open('Books.csv', 'r') as books_file:
+    books = books_file.readlines()[1:]
+    for idx, book in enumerate(books):
+        authorname = book.split(',')[2]
+        # types = set()
+
+        # types.add(type(authorname))
+
+        # try:
+        #     names = authorname.split()
+        #     print(idx, names)
+        # except:
+        #     print(authorname)
+
+        # firstname = ' '.join(names[:-1])
+        # surname = names[-1]
+        # print((firstname, surname))
--- django_tut/books/views.py
+++ django_tut/books/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+import pandas as pd
+from django.shortcuts import render, HttpResponse
 from . import models
+
+popular_df = pd.read_pickle('books/popular.pkl')
+pt = pd.read_pickle('books/pt.pkl')
+books = pd.read_pickle('books/books.pkl')
+similarity_scores = pd.read_pickle('books/similarity_scores.pkl')
 
 
 # Create your views here.
 def index(request):
-    books = list(models.Book.objects.all())
+    books_data = []
+
+    for index in books.index:
+        books_data.append({
+            'id': index,
+            'title': books['Book-Title'][index],
+            'author': books['Book-Author'][index],
+            'thumbnailurl': books['Image-URL-M'][index],
+            'quantity': 15,
+            'price': 15.99
+        })
+
     return render(request, 'books/index.html', context={
-        'books': books,
+        'books': books_data[:50],
         'user': {
             'firstname': "Mahfuzur Rahman"
         }
@@ -15,11 +32,37 @@ def index(request):
 
 
 def book_detail(request, id: int):
-    book = models.Book.objects.get(id=id)
+    book = books.iloc[id]
+
+    book = {
+        'id': id,
+        'title': book['Book-Title'],
+        'summary': 'Random Summary',
+        'author': book['Book-Author'],
+        'thumbnailurl': book['Image-URL-M'],
+        'quantity': 15,
+        'price': 15.99
+    }
+
+    similar_items = sorted(
+        list(enumerate(similarity_scores[id])), key=lambda x: x[1], reverse=True)[1:6]
+
+    similar_items_ids = [id for (id, _) in similar_items]
+    similar_books = books.iloc[similar_items_ids]
+    print(similar_books)
+
+    recommendation = [{
+        'id': index,
+        'title': books['Book-Title'][index],
+        'thumbnailurl': books['Image-URL-M'][index],
+        'quantity': 15,
+        'price': 15.99
+    } for index in similar_books.index]
 
     return render(request, 'books/book_detail.html', context={
         'book': book,
         'user': {
             'firstname': 'Mahfuzur Rahman'
-        }
+        },
+        'recommendation': recommendation
     })
