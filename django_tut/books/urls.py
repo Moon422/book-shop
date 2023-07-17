a60e9bc03490d2706f53be6e918fe79cbbb8a518
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2023-07-18 01:08:51 +0600

    full of surprises

--- django_tut/books/urls.py
+++ django_tut/books/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="book_index"),
-    path('<int:id>', views.book_detail, name="book_book_detail")
+    path('<int:id>', views.book_detail, name="book_book_detail"),
+    path('surprise', views.book_suprise, name="book_book_surprise")
 ]
--- django_tut/books/views.py
+++ django_tut/books/views.py
@@ -1,3 +1,5 @@
+import random
+import numpy as np
 import pandas as pd
 from django.shortcuts import render, HttpResponse
 from . import models
@@ -10,6 +12,8 @@ similarity_scores = pd.read_pickle('books/similarity_scores.pkl')
 
 # Create your views here.
 def index(request):
+    print('fuck')
+
     books_data = []
 
     for index in books.index:
@@ -23,7 +27,7 @@ def index(request):
         })
 
     return render(request, 'books/index.html', context={
-        'books': books_data[:50],
+        'best_sellers': books_data,
         'user': {
             'firstname': "Mahfuzur Rahman"
         }
@@ -66,3 +70,34 @@ def book_detail(request, id: int):
         },
         'recommendation': recommendation
     })
+
+
+def book_suprise(request):
+    book_title = request.GET.get('book-title')
+
+    if book_title:
+        index = np.where(pt.index == book_title)[0][0]
+        similar_items = sorted(
+            list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:18]
+
+        similar_book_ids = [item[0] for item in similar_items]
+        # print(similar_book_ids)
+
+        similar_books = books.iloc[similar_book_ids]
+
+        similar_books = [{
+            'id': index,
+            'title': books['Book-Title'][index],
+            'thumbnailurl': books['Image-URL-M'][index],
+            'quantity': 15,
+            'price': 15.99
+        } for index in similar_books.index]
+
+        print(similar_books)
+
+        return render(request, 'books/surprise.html', {
+            'book_title': book_title,
+            'similar_books': similar_books
+        })
+
+    return render(request, 'books/surprise.html')
